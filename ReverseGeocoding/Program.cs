e2180cf0dbc772add1c814b2c05516cf084e13c9
Stanko Milosev <stanko.milosev@windowslive.com>
2019-12-25 11:18:25 +0100

    method to create URL for a reverse Geocoding request

--- ReverseGeocoding/Program.cs
+++ ReverseGeocoding/Program.cs
@@ -8,6 +8,12 @@ namespace ReverseGeocoding
     {
       IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("jsconfig.json", true, true).Build();
       string googleApiKey = configuration["gapikey"];
+      string test = UrlBuilder("45.140400", "19.911776", googleApiKey);
+    }
+
+    private static string UrlBuilder(string lat, string lng, string googleApiKey)
+    {
+      return $@"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&language=en&key={googleApiKey}";
     }
   }
 }
