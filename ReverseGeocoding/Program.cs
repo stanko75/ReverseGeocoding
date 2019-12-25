164c91cbabd2c2105f4ed09fffe3bf855e9947d8
Stanko Milosev <stanko.milosev@windowslive.com>
2019-12-25 11:25:38 +0100

    GetJson method

--- ReverseGeocoding/Program.cs
+++ ReverseGeocoding/Program.cs
@@ -8,12 +8,24 @@ namespace ReverseGeocoding
     {
       IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("jsconfig.json", true, true).Build();
       string googleApiKey = configuration["gapikey"];
-      string test = UrlBuilder("45.140400", "19.911776", googleApiKey);
+      string url = UrlBuilder("45.140400", "19.911776", googleApiKey);
+      string reverseGeocodingJson = GetJson(url);
     }
 
     private static string UrlBuilder(string lat, string lng, string googleApiKey)
     {
       return $@"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&language=en&key={googleApiKey}";
+    }
+
+    private static string GetJson(string url)
+    {
+      string doc = string.Empty;
+      using (System.Net.WebClient client = new System.Net.WebClient())
+      {
+        doc = client.DownloadString(url);
+      }
+
+      return doc;
     }
   }
 }
